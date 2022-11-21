f84ec643908ae7a80abf58f86739ec550c4d592c
HoiGe <rd_idk5682@outlook.com>
2022-11-21 17:39:40 +0800

    Update Footer

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Footer from './components/Footer.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import 'uno.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const routes = [
+    { path: '/', component: App },
+    { path: '/index', component: Footer },
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+})
+
+app.use(router)
+app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import Components from 'unplugin-vue-components/vite'
+import Unocss from 'unocss/vite'
+import { presetScalpel } from 'unocss-preset-scalpel'
+import presetUno from '@unocss/preset-uno'
+import presetAttributify from '@unocss/preset-attributify'
+import presetTagify from '@unocss/preset-tagify'
+import { presetHeroPatterns } from '@julr/unocss-preset-heropatterns'
+import { presetScrollbar } from 'unocss-preset-scrollbar'
+import { presetTypography } from 'unocss'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Components(),
+    Unocss({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetTagify(),
+        presetTypography(),
+        presetScalpel(),
+        presetHeroPatterns(),
+        presetScrollbar(),
+      ],
+    }),
+  ]
 })
